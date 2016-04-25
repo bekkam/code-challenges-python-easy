a7843e8d9717762ad00d4034df3c6fc644f53fa9
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-04-25 10:12:24 -0700

    Add initial anagramofpalindrome.py

---
+++ anagramofpalindrome.py
@@ -0,0 +1,55 @@
+"""Is the word an anagram of a palindrome?
+
+A palindrome is a word that reads the same forward and backwards
+(eg, "racecar", "tacocat"). An anagram is a rescrambling of a word
+(eg for "racecar", you could rescramble this as "arceace").
+
+Determine if the given word is a re-scrambling of a palindrome.
+The word will only contain lowercase letters, a-z.
+
+Examples::
+
+    >>> is_anagram_of_palindrome("a")
+    True
+
+    >>> is_anagram_of_palindrome("ab")
+    False
+
+    >>> is_anagram_of_palindrome("aab")
+    True
+
+    >>> is_anagram_of_palindrome("arceace")
+    True
+
+    >>> is_anagram_of_palindrome("arceaceb")
+    False
+
+"""
+
+
+def is_anagram_of_palindrome(word):
+    """Is the word an anagram of a palindrome?"""
+
+    if len(word) == 1:
+        return True
+
+    seen = {}
+
+    # create a dictionary of letter counts
+    for letter in word:
+        seen.setdefault(letter, 0)
+        seen[letter] = seen[letter] + 1
+
+    odds = []
+
+    for val in seen.values():
+        if val % 2 == 1:
+            odds.append(val)
+
+    return False if len(odds) > 1 else True
+
+if __name__ == '__main__':
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TEST PASSED. AWESOMESAUCE!\n"
