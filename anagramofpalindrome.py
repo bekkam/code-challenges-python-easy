76538f26b5a12bd96231c0a84b6701c112ae628d
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-04-25 10:20:18 -0700

    Add second solution to anagramofpalindrome.py

--- anagramofpalindrome.py
+++ anagramofpalindrome.py
@@ -26,7 +26,7 @@ Examples::
 
 """
 
-
+# Solution 1: O(N)
 def is_anagram_of_palindrome(word):
     """Is the word an anagram of a palindrome?"""
 
@@ -47,6 +47,36 @@ def is_anagram_of_palindrome(word):
             odds.append(val)
 
     return False if len(odds) > 1 else True
+
+
+# Solution 2
+def is_anagram_of_palindrome(word):
+    """Is the word an anagram of a palindrome?"""
+
+    """To use less space, rather than adding odd-numbered letters to a new list,
+    we bind identifier seen_an_odd to boolean False.  We set it to True as soon
+    as a letter count in seen is an odd value.  If seen_an_odd is True, we
+    return False, because it means that there is more than one letter in word
+    that has an odd count"""
+
+    seen = {}
+
+    # create a dictionary of letter counts
+    for letter in word:
+        seen.setdefault(letter, 0)
+        seen[letter] = seen[letter] + 1
+
+    # return False if more than 1 letter has an odd count
+    seen_an_odd = False
+
+    for val in seen.values():
+        if val % 2 == 1:
+            if seen_an_odd:
+                return False
+            seen_an_odd = True
+
+    return True
+
 
 if __name__ == '__main__':
     import doctest
