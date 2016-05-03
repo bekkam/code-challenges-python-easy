b23c924034b229fcc8bbd3569ddac5cfc33e7831
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-05-03 10:07:25 -0700

    Add largestsum.py

---
+++ largestsum.py
@@ -0,0 +1,71 @@
+"""Find the subsequence with the largest sum.
+
+Given a list of integers, like:
+
+  [1, 0, 3, -8, 4, -2, 3]
+
+Return the contiguous subsequence with the largest sum. For
+that example, the answer would be [4, -2, 3], which sums to 5.
+
+
+    >>> largest_sum([1, 0, 3, -8, 4, -2, 3])
+    [4, -2, 3]
+
+    >>> largest_sum([1, 0, 3, -8, 4, -2, 3, -2])
+    [4, -2, 3]
+
+    >>> largest_sum([1, 0, 3, -8, 19, -20, 4, -2, 3, -2])
+    [19]
+
+For ties, return the first one:
+
+    >>> largest_sum([2, 2, -10, 1, 3, -20])
+    [2, 2]
+
+Return the shortest version:
+
+    >>> largest_sum([2, -2, 3, -1])
+    [3]
+
+If the list is all negative numbers, the subsequence
+with the highest sum will be empty (ie, we can do no better
+than pick nothing!):
+
+    >>> largest_sum([-1, -2])
+    []
+"""
+
+
+def largest_sum(nums):
+    """Find subsequence with largest sum."""
+
+    """
+    The max subarray is the greater of: the sum of prev subarray, or the sum of
+    the subarray at the current position.
+    The start index is reset to current position whenever the current position
+    causes the sum of the array to go negative.
+    """
+    def get_largest_sum(max_sum, array, start, end, max_start, max_stop):
+
+        # base case: we're at the end of the array
+        if end > len(nums):
+            return nums[max_start: max_stop]
+
+        if sum(array[start:end]) > max_sum:
+            max_sum = sum(array[start:end])
+            max_start = start
+            max_stop = end
+
+        # if sum of current sequence is <= 0:
+        if sum(array[start:end]) <= 0:
+            start = end
+
+        return get_largest_sum(max_sum, array, start, end + 1, max_start, max_stop)
+
+    return get_largest_sum(0, nums, 0, 0, 0, 0)
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. YOU HANDLED THIS SUMMARILY!\n"
