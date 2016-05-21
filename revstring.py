99a93c48af3cb2cdb532f04d62f8e9eebf942c89
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-05-20 20:18:26 -0700

    Add solution using range; add timer results

--- revstring.py
+++ revstring.py
@@ -14,7 +14,8 @@ For example::
 """
 
 
-def rev_string(astring):
+# Solution 1
+def rev_string1(astring):
     """Return reverse of string.
 
     You may NOT use the reversed() function!
@@ -27,8 +28,33 @@ def rev_string(astring):
 
     return result
 
-# print rev_string("porcupine")
+# Solution 2 (HB solution)
+def rev_string2(astring):
+    """Return reverse of string.
+
+    You may NOT use the reversed() function!
+    """
+
+    out = ""
+
+    for i in range(len(astring), 0, -1):
+        out += astring[i - 1]
+
+    return out
+
+
 if __name__ == '__main__':
     import doctest
+    import timeit
+
+    time1 = (timeit.timeit("rev_string1('hello')",
+             setup="from __main__ import rev_string1"))
+
+    time2 = (timeit.timeit("rev_string2('hello')",
+             setup="from __main__ import rev_string2"))
+
+    print "Timer results for Solution 1: ", time1           # 1.06165504456
+    print "Timer results for Solution 2: ", time2           # 1.75967407227
+
     if doctest.testmod().failed == 0:
         print "\n*** ALL TESTS PASSED. !KROW DOOG\n"
